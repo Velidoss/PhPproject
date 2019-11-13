aa5d01c7023513e20e343dc9e6c54691ace70880
Velidoss <godofthemountain09@gmail.com>
2019-11-13 19:41:52 +0200

    Сделана форма регистрации

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -28,5 +28,66 @@ if (isset($_POST['signup-submit'])){
         header("Location: ../signup.php?error=invaliduid&mail".$email);
         exit();
     }
+    elseif ($password == $passwordRepeat){
+        header("Location: ../signup.php?error=passwordcheckuid=".$username."&mail=".$email);
+        exit();
+    }
+    //если такой логин уже занят
+    else{
+        //знак вопроса 
+        $sql = "SELECT uidUsers FROM users WHERE uidUsers=?";
+        $stmt = mysqli_stmt_init($conn);
+        //всегда проверяем на наличие ошибок если скл запрос не сработает
+        if (!mysqli_stmt_prepare($stmt, $sql)){
+            header("Location: ../signup.php?error=sqlerror");
+            exit();
+        }
+        else{
+            //s - string, i - integer, b - blob, d - double
+            //По сути - указываем то что будем класть в датабазу - две строки - с юзернеймом, и паролем
+            mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+            //Выполняем все это
+            mysqli_stmt_execute($stmt);
+            //выводим результат
+            mysqli_stmt_store_result($stmt);
+            // Возвращаем к-во рядов
+            $resultCheck = mysqli_stmt_num_rows($stmt);
+            //если $resultCheck больше одного, то значит юзер с таким именем уже есть, выводим ошибку
+            if ($resultCheck >0){
+                header("Location: ../signup.php?error=usertaken&mail".$email);
+                exit();
+            }
+            //если $resultCheck не больше 0, значит можно вносить юзера в датабазу
+            else{
+                $sql = "INSERT INTO users (uidUsers, emailUsers, pwdUsers) VALUES (?, ?, ?)";
+                $stmt = mysqli_stmt_init($conn);
+                //опять же - проверяем запрос
+                if (!mysqli_stmt_prepare($stmt, $sql)){
+                    header("Location: ../signup.php?error=sqlerror");
+                    exit();
+                }
+                else{
+                    //Хешим пароль
+                    $hashedPwd = password_hash($password, PASSWORD_DEFAULT);
+                    //Снова берем передаваемые параметры, теперь уже 3 штуки.
+                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashedPwd);
+                    mysqli_stmt_execute($stmt);
+                    header("Location: ../signup.php?signup=success");
+                    exit();
+                }
+            }
+            
 
+        }
+    
+    }
+    //Закріваем sql  для экономии памяти и тд
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+}
+//Если юзер попытается залезть в эту форму без нажатия непосредственно кнопки "Signup"
+else{
+    header("Location: ../signup.php");
+    exit();
 }
