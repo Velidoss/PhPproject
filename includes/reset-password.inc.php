f0658d16befccaec899204d38570fa1dc333956f
Velidoss <godofthemountain09@gmail.com>
2019-12-05 17:58:00 +0200

    almost

---
+++ includes/reset-password.inc.php
@@ -0,0 +1,86 @@
+<?php
+
+if (isset($_POST["reset-password-submit"])) {
+	$selector = $_POST["selector"];
+	$validator = $_POST["validator"];
+	$password = $_POST["password"];
+	$passwordRepeat = $_POST["pwd-repeat"];
+
+	 if (empty($password) || empty($passwordRepeat)) {
+		header("Location: ../signup.php?newpwd=empty");
+		exit();
+
+	}elseif (empty($password != $passwordRepeat)){
+		header("Location: ../create-new-password.php?newpwd=pwdnotsame");
+		exit();
+	}
+
+	$currentDate = date("U");
+
+	require "dbh.inc.php";
+
+	$sql = "SELECT * FROM pwdReset WHERE pwdResetSelector=? AND pwdResetExpires >= ?";
+
+	$stmt = mysqli_stmt_init($conn);
+    if (!mysqli_stmt_prepare($stmt, $sql)){
+        echo "there was an error!";
+        exit();
+    }else{
+        mysqli_stmt_bind_param($stmt, "ss", $selector, $currentDate );
+        mysqli_stmt_execute($stmt);
+
+        $result = mysqli_stmt_get_result($stmt);
+        if (!$row = mysqli_fetch_assoc(result)) {
+        	exit();
+        }else{
+
+
+        	$tokenBin = hex2bin($validator);
+        	$tokenCheck = password_verify($tokenBin, $row["pwdResetToken"]);
+
+        	if ($tokenCheck === false) {
+        		echo "You need to re-submit your reset request.";
+        		exit();
+        	}elseif($tokenCheck === true){
+
+        		$tokenEmail =$row['pwdResetEmail'];
+
+        		$sql ="SELECT * FROM users WHERE emailUsers=?;";
+				$stmt = mysqli_stmt_init($conn);
+			    if (!mysqli_stmt_prepare($stmt, $sql)){
+			        echo "there was an error!";
+			        exit();
+			    }else{
+			    	mysqli_stmt_bind_param($stmt, "s", $tokenEmail);
+			    	mysqli_stmt_execute($stmt);
+			    	$result = mysqli_stmt_get_result($stmt);
+			        if (!$row = mysqli_fetch_assoc(result)) {
+			        	echo "there was an error!";
+			        	exit();
+			        }else{
+
+			        	$sql = "UPDATE users SET pwdUsers=? WHERE emailUsers=?";
+					    $stmt = mysqli_stmt_init($conn);
+					    if (!mysqli_stmt_prepare($stmt, $sql)){
+					        echo "there was an error!";
+					        exit();
+					    }else{
+					    	mysqli_stmt_bind_param($stmt, "ss", $tokenEmail);
+					    	mysqli_stmt_execute($stmt);
+			    }
+
+
+
+        	}
+
+        }
+    }
+
+
+}else{
+	header("Location: ../index.php");
+}
+
+
+
+?>
